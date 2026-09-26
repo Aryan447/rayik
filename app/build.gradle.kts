f20c1d03d05c1e51b0d189981702887f10dccfb0
aryan447 <singharyan4477@gmail.com>
2026-09-26 18:51:05 +0530

    Bump to v0.0.6 (VC6) with Wrapped + new aesthetics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ android {
     applicationId = "app.rayik.music"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
